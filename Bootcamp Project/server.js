4a64ae0601a6e8e8b394b95a2115517479b289de
Wouter Potgieter <wouterp@runninghill.co.za>
2022-05-17 10:23:19 +0200

    Created a basic user registration app

--- Bootcamp Project/server.js
+++ Bootcamp Project/server.js
@@ -10,6 +10,6 @@ db.once("open", () => console.log("Connected to database."));
 
 app.use(express.json());
 
-const subscribersRouter = require("./routes/subscribers");
-app.use("/subscribers", subscribersRouter);
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 app.listen(3000, () => console.log("Server started."));
