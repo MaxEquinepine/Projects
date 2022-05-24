f1fe6a318570d6db01b85926e184b3bb984a8b6f
Wouter Potgieter <wouterp@runninghill.co.za>
2022-05-24 09:51:25 +0200

    Implemented Config File into my application

---
+++ Bootcamp Project/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DBURL: process.env.DATABASE_URL,
+  ats: process.env.ACCESS_TOKEN_SECRET,
+  rts: process.env.refresh_TOKEN_SECRET,
+};
--- Bootcamp Project/server.js
+++ Bootcamp Project/server.js
@@ -10,8 +10,12 @@ const User = require("./models/user");
 const req = require("express/lib/request");
 const res = require("express/lib/response");
 let refreshTokens = [];
+let config = require("./config");
+const dbURL = config.DBURL;
+const ats = config.ats;
+const rts = config.rts;
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(dbURL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database."));
@@ -27,7 +31,8 @@ app.use("/users", usersRouter);
 app.get("/posts", authenticateToken, async (req, res) => {
   try {
     const users = await User.find();
-    res.json(users.filter((post) => post.username === req.user.name));
+    users.filter((post) => post.username === req.user.name);
+    res.json({ Username: users[0].username, ID: users[0].id });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -37,7 +42,7 @@ app.post("/token", (req, res) => {
   const refreshToken = req.body.token;
   if (refreshToken == null) return res.sendStatus(401);
   if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+  jwt.verify(refreshToken, rts, (err, user) => {
     if (err) return res.sendStatus(403);
     const accessToken = generateAccessToken({ name: user.name });
     res.json({ accessToken: accessToken });
@@ -53,13 +58,13 @@ app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
   const accessToken = generateAccessToken(user);
-  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  const refreshToken = jwt.sign(user, rts);
   refreshTokens.push(refreshToken);
   res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
 function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+  return jwt.sign(user, ats, { expiresIn: "15s" });
 }
 
 function authenticateToken(req, res, next) {
@@ -67,7 +72,7 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
     if (token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, ats, (err, user) => {
       if (err) return res.sendStatus(403);
       req.user = user;
       next();
